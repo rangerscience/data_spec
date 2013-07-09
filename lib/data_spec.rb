4a54c71eb9aba621f64c067c7085430143c91bb9
nick.barone <nick.barone@daqri.com>
2013-07-08 22:11:32 -0700

    Add "remember" to helpers

--- lib/data_spec.rb
+++ lib/data_spec.rb
@@ -5,5 +5,10 @@ require 'data_spec/matchers'
 if RSpec && RSpec.respond_to?(:configure)
   RSpec.configure do |config|
     config.include DataSpec::Matchers
+    config.include DataSpec::Helpers
   end
+end
+
+if defined?(World)
+  require 'data_spec/cucumber'
 end
---
+++ spec/data_spec/helpers.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe DataSpec do
+
+  it "can remember things for use in evaluation" do
+    time = Time.now
+    DataSpec::Helpers.remember("@var", time)
+    DataSpec::Helpers.evaluate("@var.day").should be(time)
+  end
+
+end
